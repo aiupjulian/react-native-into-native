75b813b991d88df0f4ea53250cefd88e6a89489d
Julian Aiup <julian.aiup@followoz.com>
2020-02-07 16:55:50 -0300

    Add button to show RN view on native app

--- ios/RNTest/ViewController.swift
+++ ios/RNTest/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import React
 
 class ViewController: UIViewController {
 
@@ -15,6 +16,25 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func highScoreButtonTapped(sender : UIButton) {
+      NSLog("Hello")
+      let jsCodeLocation = URL(string: "http://localhost:8081/index.bundle?platform=ios")
+      let mockData:NSDictionary = ["scores":
+          [
+              ["name":"Alex", "value":"42"],
+              ["name":"Joel", "value":"10"]
+          ]
+      ]
 
+      let rootView = RCTRootView(
+          bundleURL: jsCodeLocation!,
+          moduleName: "RNTestScreen",
+          initialProperties: mockData as [NSObject : AnyObject],
+          launchOptions: nil
+      )
+      let vc = UIViewController()
+      vc.view = rootView
+      self.present(vc, animated: true, completion: nil)
+    }
 }
 
